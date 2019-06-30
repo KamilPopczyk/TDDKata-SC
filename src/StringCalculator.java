4e6d00a8815f1de6aca9267ad3475d1d4d78fbea
KamilP <kam.popczyk@gmail.com>
2019-06-30 12:45:06 +0200

    Step 1
    Solution of step 1.

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,5 +1,34 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class StringCalculator {
     public static int add(String numbers) {
-        return -1;
+        int result = 0;
+        if(numbers.isBlank()) return result;
+        for(int number: convertNumbers(numbers)) {
+            result += number;
+        }
+
+        return result;
+    }
+
+    private static List<Integer> convertNumbers(String numbers) {
+        List<Integer> numbersList = new LinkedList<>();
+        String[] numbersArray = splitNumbers(numbers);
+        for(String number: numbersArray) {
+            int numberInt = 0;
+            try {
+                numberInt = Integer.parseInt(number);
+            } catch (NumberFormatException e) {
+                System.out.println(e);
+            }
+
+            numbersList.add(numberInt);
+        }
+        return numbersList;
+    }
+
+    private static String[] splitNumbers(String numbers) {
+        return numbers.split(",");
     }
 }
